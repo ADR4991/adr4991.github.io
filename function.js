bafde7e9945fa82075986dea4142403215e07a3e
Victor Adrian Medina Giraldo <123773392+ADR4991@users.noreply.github.com>
2023-01-27 17:15:32 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,3 @@
+document.getElementById('redes').onclick = function() {
+    document.getElementById('caja').innerHTML = "Facebook: Juan Musilli - Instagram:musillijuan11 - Twitter:musillijuan11"
+}
